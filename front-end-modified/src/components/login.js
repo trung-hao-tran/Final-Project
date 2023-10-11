b89eeef6d150596ff444609729ed600704ba78ca
icantknowmyname <z5402512@unsw.edu.au>
2023-10-11 12:26:12 +1100

    update front pages

--- front-end-modified/src/components/login.js
+++ front-end-modified/src/components/login.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Navbar from './global-components/navbar';
 import PageHeader from './global-components/page-header';
 import Login from './section-components/login';
-import CallToActionV1 from './section-components/call-to-action-v1';
-import Footer from './global-components/footer';
 
 const LoginV1 = () => {
     return <div>
