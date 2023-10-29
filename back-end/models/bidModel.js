eb2eb1aef640b5714964dafa6079d04bc56e830f
Tianyun Zhou <chrishiro123@gmail.com>
2023-10-29 16:28:27 +1100

    added bidModel

---
+++ back-end/models/bidModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const bidSchema = new Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  user_id: {
+    type: String,
+    required: true
+  },
+  task_id: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Bid', bidSchema)
