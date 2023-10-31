e31ad559b116d9e5f371a3be1fc16feb919dc699
Qingyue519 <145522563+Qingyue519@users.noreply.github.com>
2023-11-01 01:26:23 +1100

    Add files via upload

--- back-end/models/taskModel.js
+++ back-end/models/taskModel.js
@@ -56,23 +56,24 @@ const taskSchema = new Schema({
   },
   milestones:[
     {
-      title: String,
       description: String,
       date: {
         type: Date,
         default: Date.now
       },
-      completed: {
-        type: Boolean,
-        default: false
-      },
       priority: {
         type: String,
         enum: ['Low', 'Medium', 'High'],
         default: 'Medium'
+      },
+      status: {
+        type: String,
+        enum: ['Not Started', 'In Progress', 'Completed'],
+        default: 'Not Started'
       }
     }
   ]
+  
 }, { timestamps: true })
 
 module.exports = mongoose.model('Task', taskSchema)
