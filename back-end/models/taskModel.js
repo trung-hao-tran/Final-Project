f1d3082fc1d52abaf0bb3eb7aa639c1da203c850
trung-hao-tran <trunghao.tran.2000@gmail.com>
2023-11-13 05:43:03 +1100

    Milestone function

--- back-end/models/taskModel.js
+++ back-end/models/taskModel.js
@@ -1,95 +1,97 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const taskSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  categories: {
-    type: [String],
-    required: false
-  },
-  address: {
-    type: String,
-    required: false
-  },
-  time: {
-    start: {
-      type: Date,
-      required: true
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    end: {
-      type: Date,
-      required: true
-    }
-  },
-  images: {
-    type: [String],
-    required: false
-  },
-  frequency: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  user_id: {
-    type: String,
-    required: true
-  },
-  tasker_id: {
-    type: String,
-    default: "None"
-  },
-  priority: {
-    type: String,
-    enum: ['Low', 'Medium', 'High'],
-    default: 'Medium'
-  },
-  domain_knowledge: {
-    type: [String],
-    default: []
-  },
-  status: {
-    type: String,
-    enum: ['Not Started', 'In Progress', 'Completed'],
-    default: 'Not Started'
-  },
-  milestones:[
-    {
-      title: {
-        type: String,
-        require: true
-      },
-      description: {
-        type: String,
-        require: true 
-      },
-      date: {
+    description: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: [String],
+      required: false,
+    },
+    address: {
+      type: String,
+      required: false,
+    },
+    time: {
+      start: {
         type: Date,
-        default: Date.now
+        required: true,
       },
-      priority: {
-        type: String,
-        enum: ['Low', 'Medium', 'High'],
-        default: 'Medium'
+      end: {
+        type: Date,
+        required: true,
       },
-      status: {
-        type: String,
-        enum: ['Not Started', 'In Progress', 'Completed'],
-        default: 'Not Started'
-      }
-    }
-  ]
-  
-}, { timestamps: true })
+    },
+    images: {
+      type: [String],
+      required: false,
+    },
+    frequency: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    user_id: {
+      type: String,
+      required: true,
+    },
+    tasker_id: {
+      type: String,
+      default: "None",
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+    domain_knowledge: {
+      type: [String],
+      default: [],
+    },
+    status: {
+      type: String,
+      enum: ["Not Started", "In Progress", "Completed"],
+      default: "Not Started",
+    },
+    milestones: [
+      {
+        title: {
+          type: String,
+          require: true,
+        },
+        description: {
+          type: String,
+          require: true,
+        },
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+        priority: {
+          type: String,
+          enum: ["Low", "Medium", "High"],
+          default: "Medium",
+        },
+        status: {
+          type: String,
+          enum: ["Not Started", "In Progress", "Completed"],
+          default: "Not Started",
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Task', taskSchema)
+module.exports = mongoose.model("Task", taskSchema);
