abcbfd464778dfd2df876a9103e62179e1a9bed3
Tianyun Zhou <chrishiro123@gmail.com>
2023-11-13 22:24:19 +1100

    initialized backend tests

--- back-end/controllers/taskController.js
+++ back-end/controllers/taskController.js
@@ -180,7 +180,7 @@ const updateTask = async (req, res) => {
 
 // GET all taskers bidding on a given task
 const getTaskers = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No such task" });
   }
